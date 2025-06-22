1ded7b66b95559d66a7f1b75c4b744512e9565cc
GGgurov <newgurov2022@gmail.com>
2025-06-22 20:03:20 +0600

    Завершил проект

--- js/main.js
+++ js/main.js
@@ -16,21 +16,32 @@ window.addEventListener("DOMContentLoaded", () => {
 
     tasks.forEach(task => {
         const taskHTML = `
-        <div class="task">
-            <div class="name-task">
-                <input type="checkbox" class="checkbox">
-                <p class="task-name">${task.name}</p>
-            </div>
-            <p class="discription">${task.description}</p>
-            <div class="task-date">
-                <img class="calendar" src="./img/calendar.png" alt="calendar"> 
-                <p class="date">${task.date}</p>
-                <img class="redact" src="./img/redact.png" alt="edit task">
-            </div>
+    <div class="task">
+        <div class="name-task">
+            <input type="checkbox" class="checkbox">
+            <p class="task-name">${task.name}</p>
+            <button class="close-btn">&times;</button>
         </div>
-        `;
+        <p class="discription">${task.description}</p>
+        <div class="task-date">
+            <img class="calendar" src="./img/calendar.png" alt="calendar"> 
+            <p class="date">${task.date}</p>
+        </div>
+    </div>
+`;
+        
         taskListContainer.insertAdjacentHTML("beforeend", taskHTML);
     });
+
+    document.querySelectorAll('.close-btn').forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        const updatedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+        updatedTasks.splice(index, 1);
+        localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+        location.reload();
+    });
+});
+
 });
 
 
